0d00f72baab11cc95a3a6241e84c96bf93846521
Jeanlueqa <113385356+Jeanlueqa@users.noreply.github.com>
2022-10-21 19:06:49 +0100

    Add files via upload

---
+++ python_app.py
@@ -0,0 +1,27 @@
+from email.message import EmailMessage  # import email.message
+import ssl
+import smtplib  # this library will send email
+
+# this is where the sender's email is set
+email_sender = 'jeanluengama@gmail.com'
+# this is where the sender's passwork is set generated from Google Security app
+email_password = 'gaqmyvugrnlwkdzm'
+
+email_receiver = 'relogi5956@3mkz.com'
+
+subject = "Learnign Python with free code camp"
+body = """
+Learn Python every day it's good for you
+"""
+# create email
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+# smtplib will log in and send email to the reciever
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
